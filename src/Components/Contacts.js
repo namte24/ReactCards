40f88ffeea4fd2853202651877a348ce9974943a
Tenzing Namgyal Bhutia <79127818+namte24@users.noreply.github.com>
2023-01-10 12:49:03 +0530

    Initial commit

---
+++ src/Components/Contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    name: "Beyonce",
+    imageURL:
+      "https://www.stuttgarter-nachrichten.de/media.media.1a48f49e-1c9a-4916-a6eb-8d20f52de97c.original1024.jpg",
+    phone: "+123 456 789",
+    email: "beyonce@email.com"
+  },
+  {
+    name: "Drake",
+    imageURL:
+      "https://media1.popsugar-assets.com/files/thumbor/zan-t_Me63if8oqWYE9ENiPLlhA/0x224:2826x3050/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2020/02/11/894/n/1922398/87f6bb525e430e7bd44e40.22278576_/i/Drake.jpg",
+    phone: "+123 456 789",
+    email: "drake@email.com"
+  },
+  {
+    name: "Ana De Armas",
+    imageURL:
+      "https://hips.hearstapps.com/hmg-prod/images/ana-de-armas-announcement-image-no-logo-global-print-and-online-use-expiry-feb-2022-1614789852.jpg?crop=1xw:1xh;center,top&resize=1200:*",
+    phone: "+123 456 789",
+    email: "anae@email.com"
+  }
+];
+
+export default contacts;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import react from "react";
+import ReactDOM from "react-dom";
+import StyleSheet from "../public/styles.css";
+import App from "./Components/App";
+
+ReactDOM.render(
+  <div>
+    <h1>My Contacts</h1>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
